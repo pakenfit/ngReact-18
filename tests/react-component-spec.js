3850b78ffc65a43345516bbe2a4f2a4dfd9836a2
Pakenfit <mamadembele7@gmail.com>
2024-01-29 15:19:19 +0100

    feat: add react 18 support

--- tests/react-component-spec.js
+++ tests/react-component-spec.js
@@ -4,39 +4,48 @@
 require('es5-shim');
 require('../ngReact');
 
-var React = require( 'react' );
-var ReactTestUtils = require( 'react-addons-test-utils' );
-var ReactDOM = require( 'react-dom' );
-var angular = require( 'angular' );
-require( 'angular-mocks' );
+var React = require('react');
+var ReactTestUtils = require('react-dom/test-utils');
+var ReactDOM = require('react-dom');
+var angular = require('angular');
+require('angular-mocks');
+var PropTypes = require('prop-types');
 
-var Hello = React.createClass({
-  propTypes: {
-    fname : React.PropTypes.string,
-    lname : React.PropTypes.string,
-    changeName: React.PropTypes.func
-  },
+function Hello({ fname, lname, changeName }) {
+  const handleClick = () => {
+    var value = changeName();
+    if (value) {
+      window.GlobalChangeNameValue = value;
+    }
+  };
 
-  handleClick(){
-    this.props.changeName();
-  },
+  return (
+    <div onClick={handleClick}>
+      Hello {fname} {lname}
+      {undeclared}
+    </div>
+  );
+}
 
-  render(){
-    var {fname, lname} = this.props;
-    return <div onClick={this.handleClick}>Hello {fname} {lname}</div>;
-  }
-});
+Hello.propTypes = {
+  fname: PropTypes.string,
+  lname: PropTypes.string,
+  changeName: PropTypes.func,
+};
 
 describe('react-component', () => {
-
   var compileElement, provide;
 
   beforeEach(angular.mock.module('react'));
 
-  beforeEach(angular.mock.module(($provide) => {provide = $provide;}));
+  beforeEach(
+    angular.mock.module($provide => {
+      provide = $provide;
+    })
+  );
 
   beforeEach(inject(($rootScope, $compile) => {
-    compileElement = ( html, scope ) => {
+    compileElement = (html, scope) => {
       scope = scope || $rootScope;
       var elm = angular.element(html);
       $compile(elm)(scope);
@@ -46,11 +55,10 @@ describe('react-component', () => {
   }));
 
   describe('creation', () => {
-
     beforeEach(() => {
       window.GlobalHello = Hello;
       window.Views = {
-        Hello: Hello
+        Hello: Hello,
       };
       provide.value('InjectedHello', Hello);
     });
@@ -61,28 +69,27 @@ describe('react-component', () => {
     });
 
     it('should create global component with name', () => {
-      var elm = compileElement( '<react-component name="GlobalHello"/>');
+      var elm = compileElement('<react-component name="GlobalHello"/>');
       expect(elm.text().trim()).toEqual('Hello');
     });
 
     it('should create global component with nested name', () => {
-      var elm = compileElement( '<react-component name="Views.Hello"/>');
+      var elm = compileElement('<react-component name="Views.Hello"/>');
       expect(elm.text().trim()).toEqual('Hello');
     });
 
     it('should create injectable component with name', () => {
-      var elm = compileElement( '<react-component name="InjectedHello"/>' );
+      var elm = compileElement('<react-component name="InjectedHello"/>');
       expect(elm.text().trim()).toEqual('Hello');
     });
   });
 
   describe('properties', () => {
-
     beforeEach(() => {
       provide.value('Hello', Hello);
     });
 
-    it('should bind to properties on scope', inject(($rootScope) => {
+    it('should bind to properties on scope', inject($rootScope => {
       var scope = $rootScope.$new();
       scope.person = { fname: 'Clark', lname: 'Kent' };
 
@@ -93,8 +100,7 @@ describe('react-component', () => {
       expect(elm.text().trim()).toEqual('Hello Clark Kent');
     }));
 
-    it('should rerender when scope is updated', inject(($rootScope) => {
-
+    it('should rerender when scope is updated', inject($rootScope => {
       var scope = $rootScope.$new();
       scope.person = { fname: 'Clark', lname: 'Kent' };
 
@@ -112,15 +118,15 @@ describe('react-component', () => {
       expect(elm.text().trim()).toEqual('Hello Bruce Banner');
     }));
 
-    it('should accept callbacks on scope', inject(($rootScope) => {
-
+    it('should accept callbacks on scope', inject($rootScope => {
       var scope = $rootScope.$new();
       scope.person = {
-        fname: 'Clark', lname: 'Kent',
+        fname: 'Clark',
+        lname: 'Kent',
         changeName: () => {
           scope.person.fname = 'Bruce';
           scope.person.lname = 'Banner';
-        }
+        },
       };
 
       var elm = compileElement(
@@ -129,23 +135,23 @@ describe('react-component', () => {
       );
       expect(elm.text().trim()).toEqual('Hello Clark Kent');
 
-      ReactTestUtils.Simulate.click( elm[0].firstChild );
+      ReactTestUtils.Simulate.click(elm[0].firstChild);
 
       expect(elm.text().trim()).toEqual('Hello Bruce Banner');
     }));
 
-    it('should scope.$apply() callback invocations made after changing props directly', inject(($rootScope) => {
+    it('should scope.$apply() callback invocations made after changing props directly', inject($rootScope => {
       var scope = $rootScope.$new();
       scope.changeCount = 0;
       scope.person = {
-        fname: 'Clark', lname: 'Kent',
+        fname: 'Clark',
+        lname: 'Kent',
         changeName: () => {
           scope.changeCount += 1;
-        }
+        },
       };
 
-      var template =
-        `<div>
+      var template = `<div>
           <p>{{changeCount}}</p>
           <react-component name="Hello" props="person"/>
         </div>`;
@@ -155,7 +161,7 @@ describe('react-component', () => {
       expect(elm.children().eq(0).text().trim()).toEqual('0');
 
       // first callback invocation
-      ReactTestUtils.Simulate.click( elm[0].children.item(1).lastChild );
+      ReactTestUtils.Simulate.click(elm[0].children.item(1).lastChild);
 
       expect(elm.children().eq(0).text().trim()).toEqual('1');
 
@@ -166,100 +172,99 @@ describe('react-component', () => {
       expect(elm.children().eq(0).text().trim()).toEqual('1');
 
       // second callback invocation
-      ReactTestUtils.Simulate.click( elm[0].children.item(1).lastChild );
+      ReactTestUtils.Simulate.click(elm[0].children.item(1).lastChild);
 
       expect(elm.children().eq(0).text().trim()).toEqual('2');
     }));
   });
 
   describe('watch-depth', () => {
-
     describe('value', () => {
       var elm, scope;
 
-      beforeEach(inject(($rootScope) => {
+      beforeEach(inject($rootScope => {
         provide.value('Hello', Hello);
         scope = $rootScope.$new();
         scope.person = { fname: 'Clark', lname: 'Kent' };
       }));
 
-      it('should rerender when a property of scope object is updated', () => inject(() => {
-
-        elm = compileElement(
+      it('should rerender when a property of scope object is updated', () =>
+        inject(() => {
+          elm = compileElement(
             '<react-component name="Hello" props="person" watch-depth="value"/>',
-            scope);
+            scope
+          );
 
-        expect(elm.text().trim()).toEqual('Hello Clark Kent');
+          expect(elm.text().trim()).toEqual('Hello Clark Kent');
 
-        scope.person.fname = 'Bruce';
-        scope.person.lname = 'Banner';
-        scope.$apply();
+          scope.person.fname = 'Bruce';
+          scope.person.lname = 'Banner';
+          scope.$apply();
 
-        expect(elm.text().trim()).toEqual('Hello Bruce Banner');
-      }));
+          expect(elm.text().trim()).toEqual('Hello Bruce Banner');
+        }));
 
-      it('should rerender when a property of scope object is updated', () => inject(() => {
-
-        //watch-depth will default to value
-        elm = compileElement(
+      it('should rerender when a property of scope object is updated', () =>
+        inject(() => {
+          //watch-depth will default to value
+          elm = compileElement(
             '<react-component name="Hello" props="person" watch-depth="blahblah"/>',
-            scope);
+            scope
+          );
 
-        expect(elm.text().trim()).toEqual('Hello Clark Kent');
+          expect(elm.text().trim()).toEqual('Hello Clark Kent');
 
-        scope.person.fname = 'Bruce';
-        scope.person.lname = 'Banner';
-        scope.$apply();
+          scope.person.fname = 'Bruce';
+          scope.person.lname = 'Banner';
+          scope.$apply();
 
-        expect(elm.text().trim()).toEqual('Hello Bruce Banner');
-      }));
+          expect(elm.text().trim()).toEqual('Hello Bruce Banner');
+        }));
     });
 
     describe('reference', () => {
       var elm, scope;
 
-      beforeEach(inject(($rootScope) => {
+      beforeEach(inject($rootScope => {
         provide.value('Hello', Hello);
         scope = $rootScope.$new();
         scope.person = { fname: 'Clark', lname: 'Kent' };
 
         elm = compileElement(
-            '<react-component name="Hello" props="person" watch-depth="reference"/>',
-            scope);
+          '<react-component name="Hello" props="person" watch-depth="reference"/>',
+          scope
+        );
       }));
 
-      it('should rerender when scope object is updated', () => inject(() => {
+      it('should rerender when scope object is updated', () =>
+        inject(() => {
+          expect(elm.text().trim()).toEqual('Hello Clark Kent');
 
-        expect(elm.text().trim()).toEqual('Hello Clark Kent');
+          scope.person = { fname: 'Bruce', lname: 'Banner' };
+          scope.$apply();
 
-        scope.person = { fname: 'Bruce', lname: 'Banner' };
-        scope.$apply();
+          expect(elm.text().trim()).toEqual('Hello Bruce Banner');
+        }));
 
-        expect(elm.text().trim()).toEqual('Hello Bruce Banner');
-      }));
+      it('should NOT rerender when a property of scope object is updated', () =>
+        inject(() => {
+          expect(elm.text().trim()).toEqual('Hello Clark Kent');
 
-      it('should NOT rerender when a property of scope object is updated', () => inject(() => {
+          scope.person.fname = 'Bruce';
+          scope.person.lname = 'Banner';
+          scope.$apply();
 
-        expect(elm.text().trim()).toEqual('Hello Clark Kent');
-
-        scope.person.fname = 'Bruce';
-        scope.person.lname = 'Banner';
-        scope.$apply();
-
-        expect(elm.text().trim()).toEqual('Hello Clark Kent');
-      }));
+          expect(elm.text().trim()).toEqual('Hello Clark Kent');
+        }));
     });
-
   });
 
   describe('destruction', () => {
-
     beforeEach(() => {
       provide.value('Hello', Hello);
     });
 
-    it('should unmount component when scope is destroyed', inject(($rootScope) => {
-
+    it('should unmount component when scope is destroyed', inject($rootScope => {
       var scope = $rootScope.$new();
       var elm = compileElement(
         '<react-component name="Hello" props="person"/>',
@@ -274,13 +279,12 @@ describe('react-component', () => {
     }));
   });
 
-  describe('deferred destruction', function() {
-
+  describe('deferred destruction', function () {
     beforeEach(() => {
       provide.value('Hello', Hello);
     });
 
-    it('should not unmount component when scope is destroyed', inject(($rootScope) => {
+    it('should not unmount component when scope is destroyed', inject($rootScope => {
       var scope = $rootScope.$new();
       scope.person = { firstName: 'Clark', lastName: 'Kent' };
       scope.callback = jasmine.createSpy('callback');
@@ -299,13 +303,13 @@ describe('react-component', () => {
       expect(scope.callback.calls.count()).toEqual(1);
     }));
 
-    it('should pass unmount function as a "unmountComponent" parameter to callback', inject(($rootScope) => {
+    it('should pass unmount function as a "unmountComponent" parameter to callback', inject($rootScope => {
       var scope = $rootScope.$new();
       scope.person = { firstName: 'Clark', lastName: 'Kent' };
-      scope.callback = function(unmountFn) {
+      scope.callback = function (unmountFn) {
         unmountFn();
       };
-      
+
       spyOn(scope, 'callback').and.callThrough();
 
       var elm = compileElement(
